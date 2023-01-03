71c3ddf543c31787ea48b4148780720d0749ca95
Ziv <95753868+Zivgl66@users.noreply.github.com>
2023-01-03 20:34:56 +0200

    Add files via upload

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,7 +4,7 @@ import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 const FooterComponent = () => {
   return (
     <MDBFooter
-      className=" text-center text-white footer container-fluid"
+      className="footer_container text-center text-white footer container-fluid"
       style={{ borderRadius: "25px" }}
     >
       <MDBContainer className="p-1 pb-0">
